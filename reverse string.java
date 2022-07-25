bd3d52c7eb3a30bca125f526adc8b86641a9012b
Hissamuddin11 <109800747+Hissamuddin11@users.noreply.github.com>
2022-07-25 13:02:07 +0530

    Add files via upload

---
+++ reverse string.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[]) 
+    {
+        
+        String str = "thisisstring";
+        
+        String rev = new StringBuilder(str).reverse().toString();
+        
+        System.out.println(rev);
+        
+        
+    }
+}
