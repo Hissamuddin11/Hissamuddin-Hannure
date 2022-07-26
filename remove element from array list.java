1a3a1a65154d03eafcf0a10b4a0b1e1462df39ea
Hissamuddin11 <109800747+Hissamuddin11@users.noreply.github.com>
2022-07-26 11:40:14 +0530

    Add files via upload

---
+++ remove element from array list.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main  
+{  
+  
+    public static void main(String args[])  
+    {  
+        
+        //Create the ArrayList
+        List<Integer> al = new ArrayList<>(); 
+        //Add the items
+        al.add(10);
+        al.add(18);
+        al.add(28);
+        al.add(68);
+        al.add(48);
+        System.out.println(al);
+        al.remove(1); 
+    System.out.println(al);
+    
+    }
+    
+}
+
